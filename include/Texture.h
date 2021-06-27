badee165d7812971dcb24eafaa44fa1ca9c7719d
Ian <824999404@qq.com>
2021-06-27 14:20:13 +0800

    Add CubmapTexture;sample3d todo

--- include/Texture.h
+++ include/Texture.h
@@ -13,5 +13,20 @@ public:
 	glm::vec4 sample(const glm::vec2& uv);   /*ÎÆÀí²ÉÑù*/
 	glm::vec4 sample(float u,float v);
 };
+class CubemapTexture {
+private:
+	unsigned char* data[6];
+	const char* fileNames[6];
+	int width;
+	int height;
+public:
+	void loadTextures(const char* posXFilename, const char* negXFilename,
+		const char* posYFilename, const char* negYFilename,
+		const char* posZFilename, const char* negZFilename);
+	void loadTexture(unsigned int index);
+	glm::vec4 sample(float u, float v, unsigned index);
+	glm::vec4 sample(const glm::vec2& uv, unsigned index);
+	glm::vec4 sample3D(const glm::vec3& dir);
+};
 
 #endif //__TEXTURE_H__
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -36,3 +36,60 @@ glm::vec4 Texture2D::sample(float u, float v){
 glm::vec4 Texture2D::sample(const glm::vec2& uv) {
    return sample(uv.x, uv.y);
 }
+
+
+void CubemapTexture::loadTextures(const char* posXFilename, const char* negXFilename,
+	const char* posYFilename, const char* negYFilename,
+	const char* posZFilename, const char* negZFilename) {
+
+	fileNames[0] = posXFilename;
+	fileNames[1] = negXFilename;
+	fileNames[2] = posYFilename;
+	fileNames[3] = negYFilename;
+	fileNames[4] = posZFilename;
+	fileNames[5] = negZFilename;
+
+	for (unsigned int i = 0; i < 6; i++) {
+		loadTexture(i);
+	}
+
+
+}
+
+void CubemapTexture::loadTexture(unsigned int index) {
+	int n;
+	unsigned char* tmp = stbi_load(fileNames[index], &this->width, &this->height, &n, 0);
+	data[index] = new unsigned char[4 * width*height];
+	for (int i = 0; i < width*height; i++) {
+		if (n == 1) {
+			memset(data[index] + 4 * i, tmp[i], sizeof(unsigned char) * 4);
+		}
+		else if (n == 3) {
+			memcpy(data[index] + 4 * i, tmp + 3 * i, sizeof(unsigned char) * 3);
+			data[index][4 * i + 3] = 255;
+		}
+		else if (n == 4) {
+			memcpy(data[index] + 4 * i, tmp + 4 * i, sizeof(unsigned char) * 4);
+		}
+	}
+	stbi_image_free(tmp);
+	std::cout << "Load Texture From \"" << fileNames[index] << "\" Successfully!\n";
+}
+
+
+glm::vec4 CubemapTexture::sample(float u, float v, unsigned index) {
+	int x = (int)(u * width - 0.5f) % width;
+	int y = (int)(v * height - 0.5f) % height;
+	x = x < 0 ? width + x : x;
+	y = y < 0 ? height + y : y;
+	return VecColor::color_256_to_vec(data[index] + 4 * (x + (height - 1 - y) * width - 1));
+}
+glm::vec4 CubemapTexture::sample(const glm::vec2& uv, unsigned index) {
+	return sample(uv.x, uv.y, index);
+
+}
+
+glm::vec4 CubemapTexture::sample3D(const glm::vec3& dir) {
+	//TODO: cube映射为球体->球体求交->采样
+	return glm::vec4(0);
+}
