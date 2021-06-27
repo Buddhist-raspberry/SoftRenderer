bde7575f34742e65473bb9298e1cced03e1370c3
Ian <824999404@qq.com>
2021-06-27 23:04:00 +0800

    Add SpotLight

--- include/Light.h
+++ include/Light.h
@@ -34,8 +34,9 @@ public:
 	glm::vec3 GetDirection(const glm::vec3& pos) override;
 };
 
+//点光源
 class PointLight :public Light {
-private:
+protected:
 	glm::vec3 position;
 	float constant;
 	float linear;
@@ -53,5 +54,22 @@ public:
 	glm::vec3 GetDirection(const glm::vec3& pos) override;
 };
 
+//聚光灯
+class SpotLight :public PointLight {
+private:
+	glm::vec3 direction;
+	float cutOff;
+	float outerCutOff;
+public:
+	SpotLight(glm::vec3 _color, float _indensity, glm::vec3 _position, glm::vec3 _direction,
+		float _constant, float _linear, float _exp, float _cutOffAngle, float _outerCutOffAngle)
+		:PointLight(_color, _indensity,_position,_constant,_linear,_exp) {
+		direction = _direction;
+		cutOff = glm::cos(glm::radians(_cutOffAngle));
+		outerCutOff = glm::cos(glm::radians(_outerCutOffAngle));
+	}
+	glm::vec3 GetColor(const glm::vec3& pos);
+
+};
 
 #endif // !__LIGHT_H__
